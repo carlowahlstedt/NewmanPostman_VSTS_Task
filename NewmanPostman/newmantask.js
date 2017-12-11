d1e47c8b42aaae1acf8fbb1a2a98151362b0eeaa
Nick DeMayo <sunmorgus@gmail.com>
2017-12-11 13:57:38 -0500

    - Added ability to specify a folder for collection files;

--- NewmanPostman/newmantask.js
+++ NewmanPostman/newmantask.js
@@ -16,9 +16,6 @@ function run() {
             tl.debug('executing newman');
             tl.setResourcePath(path.join(__dirname, 'task.json'));
             var newman = tl.tool(tl.which('newman', true));
-            newman.arg('run');
-            newman.arg(tl.getPathInput('collectionFileSource', true, true));
-            newman.arg(['-e', tl.getPathInput('environment', true, true)]);
             let sslClientCert = tl.getPathInput('sslClientCert', false, true);
             newman.argIf(typeof sslClientCert != 'undefined' && sslClientCert, ['--ssl-client-cert', sslClientCert]);
             let sslClientKey = tl.getPathInput('sslClientKey', false, true);
@@ -37,7 +34,29 @@ function run() {
             newman.argIf(typeof timeoutRequest != 'undefined' && timeoutRequest, ['--timeout-request', timeoutRequest]);
             let numberOfIterations = tl.getInput('numberOfIterations');
             newman.argIf(typeof numberOfIterations != 'undefined' && numberOfIterations, ['-n', numberOfIterations]);
-            yield newman.exec();
+            let collectionFileSource = tl.getPathInput('collectionFileSource', true, true);
+            if (tl.stats(collectionFileSource).isDirectory()) {
+                let contents = tl.getDelimitedInput('Contents', '\n', true);
+                collectionFileSource = path.normalize(collectionFileSource);
+                let allPaths = tl.find(collectionFileSource);
+                let matchedPaths = tl.match(allPaths, contents, collectionFileSource);
+                let matchedFiles = matchedPaths.filter((itemPath) => !tl.stats(itemPath).isDirectory());
+                console.log(tl.loc('FoundNFiles', matchedFiles.length));
+                if (matchedFiles.length > 0) {
+                    matchedFiles.forEach((file) => __awaiter(this, void 0, void 0, function* () {
+                        newman.arg('run');
+                        newman.arg(file);
+                        newman.arg(['-e', tl.getPathInput('environment', true, true)]);
+                        yield newman.exec();
+                    }));
+                }
+            }
+            else {
+                newman.arg('run');
+                newman.arg(collectionFileSource);
+                newman.arg(['-e', tl.getPathInput('environment', true, true)]);
+                yield newman.exec();
+            }
             tl.setResult(tl.TaskResult.Succeeded, "Success");
         }
         catch (err) {
--- NewmanPostman/newmantask.ts
+++ NewmanPostman/newmantask.ts
@@ -9,10 +9,6 @@ async function run() {
 
         var newman: trm.ToolRunner = tl.tool(tl.which('newman', true));
 
-        newman.arg('run');
-        newman.arg(tl.getPathInput('collectionFileSource', true, true));
-        newman.arg(['-e', tl.getPathInput('environment', true, true)]);
-
         let sslClientCert = tl.getPathInput('sslClientCert', false, true);
         newman.argIf(typeof sslClientCert != 'undefined' && sslClientCert, ['--ssl-client-cert', sslClientCert]);
 
@@ -40,7 +36,34 @@ async function run() {
         let numberOfIterations = tl.getInput('numberOfIterations');
         newman.argIf(typeof numberOfIterations != 'undefined' && numberOfIterations, ['-n', numberOfIterations]);
 
-        await newman.exec();
+        let collectionFileSource = tl.getPathInput('collectionFileSource', true, true);
+        if (tl.stats(collectionFileSource).isDirectory()) {
+            let contents: string[] = tl.getDelimitedInput('Contents', '\n', true);
+            collectionFileSource = path.normalize(collectionFileSource);
+
+            let allPaths: string[] = tl.find(collectionFileSource);
+            let matchedPaths: string[] = tl.match(allPaths, contents, collectionFileSource);
+            let matchedFiles: string[] = matchedPaths.filter((itemPath: string) => !tl.stats(itemPath).isDirectory());
+
+            console.log(tl.loc('FoundNFiles', matchedFiles.length));
+
+            if (matchedFiles.length > 0) {
+                matchedFiles.forEach(async (file: string) => {
+                    newman.arg('run');
+                    newman.arg(file);
+                    newman.arg(['-e', tl.getPathInput('environment', true, true)]);
+
+                    await newman.exec();
+                });
+            }
+        }
+        else {
+            newman.arg('run');
+            newman.arg(collectionFileSource);
+            newman.arg(['-e', tl.getPathInput('environment', true, true)]);
+
+            await newman.exec();
+        }
 
         tl.setResult(tl.TaskResult.Succeeded, "Success");
     } catch (err) {

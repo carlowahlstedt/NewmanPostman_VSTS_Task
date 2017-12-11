ed44bee7a5adab22c2ec9ddd6b5dfa3636e40f67
Nick DeMayo <sunmorgus@gmail.com>
2017-12-11 14:56:45 -0500

    - Make multiple runs happen in sync (it was running them all at the same time before, which was causing issues); - Create new runner per collection file found; - Fix "found files" console.log message; - Properly return failed task status if one of the collections fails;

--- NewmanPostman/newmantask.js
+++ NewmanPostman/newmantask.js
@@ -10,54 +10,68 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 const path = require("path");
 const tl = require("vsts-task-lib/task");
+function GetToolRunner() {
+    var newman = tl.tool(tl.which('newman', true));
+    let sslClientCert = tl.getPathInput('sslClientCert', false, true);
+    newman.argIf(typeof sslClientCert != 'undefined' && sslClientCert, ['--ssl-client-cert', sslClientCert]);
+    let sslClientKey = tl.getPathInput('sslClientKey', false, true);
+    newman.argIf(typeof sslClientKey != 'undefined' && sslClientKey, ['--ssl-client-key', sslClientKey]);
+    let reporterHtmlTemplate = tl.getPathInput('reporterHtmlTemplate', false, true);
+    newman.argIf(typeof reporterHtmlTemplate != 'undefined' && reporterHtmlTemplate, ['--reporter-html-template', reporterHtmlTemplate]);
+    let reporterHtmlExport = tl.getInput('reporterHtmlExport');
+    newman.argIf(typeof reporterHtmlExport != 'undefined' && reporterHtmlExport, ['--reporter-html-export', reporterHtmlExport]);
+    let reporterJsonExport = tl.getInput('reporterJsonExport');
+    newman.argIf(typeof reporterJsonExport != 'undefined' && reporterJsonExport, ['--reporter-json-export', reporterJsonExport]);
+    let reporters = tl.getInput('reporters');
+    newman.argIf(typeof reporters != 'undefined' && reporters, ['-r', reporters]);
+    let delayRequest = tl.getInput('delayRequest');
+    newman.argIf(typeof delayRequest != 'undefined' && delayRequest, ['--delay-request', delayRequest]);
+    let timeoutRequest = tl.getInput('timeoutRequest');
+    newman.argIf(typeof timeoutRequest != 'undefined' && timeoutRequest, ['--timeout-request', timeoutRequest]);
+    let numberOfIterations = tl.getInput('numberOfIterations');
+    newman.argIf(typeof numberOfIterations != 'undefined' && numberOfIterations, ['-n', numberOfIterations]);
+    return newman;
+}
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             tl.debug('executing newman');
             tl.setResourcePath(path.join(__dirname, 'task.json'));
-            var newman = tl.tool(tl.which('newman', true));
-            let sslClientCert = tl.getPathInput('sslClientCert', false, true);
-            newman.argIf(typeof sslClientCert != 'undefined' && sslClientCert, ['--ssl-client-cert', sslClientCert]);
-            let sslClientKey = tl.getPathInput('sslClientKey', false, true);
-            newman.argIf(typeof sslClientKey != 'undefined' && sslClientKey, ['--ssl-client-key', sslClientKey]);
-            let reporterHtmlTemplate = tl.getPathInput('reporterHtmlTemplate', false, true);
-            newman.argIf(typeof reporterHtmlTemplate != 'undefined' && reporterHtmlTemplate, ['--reporter-html-template', reporterHtmlTemplate]);
-            let reporterHtmlExport = tl.getInput('reporterHtmlExport');
-            newman.argIf(typeof reporterHtmlExport != 'undefined' && reporterHtmlExport, ['--reporter-html-export', reporterHtmlExport]);
-            let reporterJsonExport = tl.getInput('reporterJsonExport');
-            newman.argIf(typeof reporterJsonExport != 'undefined' && reporterJsonExport, ['--reporter-json-export', reporterJsonExport]);
-            let reporters = tl.getInput('reporters');
-            newman.argIf(typeof reporters != 'undefined' && reporters, ['-r', reporters]);
-            let delayRequest = tl.getInput('delayRequest');
-            newman.argIf(typeof delayRequest != 'undefined' && delayRequest, ['--delay-request', delayRequest]);
-            let timeoutRequest = tl.getInput('timeoutRequest');
-            newman.argIf(typeof timeoutRequest != 'undefined' && timeoutRequest, ['--timeout-request', timeoutRequest]);
-            let numberOfIterations = tl.getInput('numberOfIterations');
-            newman.argIf(typeof numberOfIterations != 'undefined' && numberOfIterations, ['-n', numberOfIterations]);
             let collectionFileSource = tl.getPathInput('collectionFileSource', true, true);
+            var taskSuccess = true;
             if (tl.stats(collectionFileSource).isDirectory()) {
                 let contents = tl.getDelimitedInput('Contents', '\n', true);
                 collectionFileSource = path.normalize(collectionFileSource);
                 let allPaths = tl.find(collectionFileSource);
                 let matchedPaths = tl.match(allPaths, contents, collectionFileSource);
                 let matchedFiles = matchedPaths.filter((itemPath) => !tl.stats(itemPath).isDirectory());
-                console.log(tl.loc('FoundNFiles', matchedFiles.length));
+                console.log("found %d files", matchedFiles.length);
                 if (matchedFiles.length > 0) {
-                    matchedFiles.forEach((file) => __awaiter(this, void 0, void 0, function* () {
+                    matchedFiles.forEach((file) => {
+                        var newman = GetToolRunner();
                         newman.arg('run');
                         newman.arg(file);
                         newman.arg(['-e', tl.getPathInput('environment', true, true)]);
-                        yield newman.exec();
-                    }));
+                        var execResponse = newman.execSync();
+                        if (execResponse.code === 1) {
+                            taskSuccess = false;
+                        }
+                    });
                 }
             }
             else {
+                var newman = GetToolRunner();
                 newman.arg('run');
                 newman.arg(collectionFileSource);
                 newman.arg(['-e', tl.getPathInput('environment', true, true)]);
                 yield newman.exec();
             }
-            tl.setResult(tl.TaskResult.Succeeded, "Success");
+            if (taskSuccess) {
+                tl.setResult(tl.TaskResult.Succeeded, "Success");
+            }
+            else {
+                tl.setResult(tl.TaskResult.Failed, "Failed");
+            }
         }
         catch (err) {
             tl.setResult(tl.TaskResult.Failed, err.message);
--- NewmanPostman/newmantask.ts
+++ NewmanPostman/newmantask.ts
@@ -2,41 +2,36 @@ import path = require('path');
 import tl = require('vsts-task-lib/task');
 import trm = require('vsts-task-lib/toolrunner');
 
+function GetToolRunner() {
+    var newman: trm.ToolRunner = tl.tool(tl.which('newman', true));
+    let sslClientCert = tl.getPathInput('sslClientCert', false, true);
+    newman.argIf(typeof sslClientCert != 'undefined' && sslClientCert, ['--ssl-client-cert', sslClientCert]);
+    let sslClientKey = tl.getPathInput('sslClientKey', false, true);
+    newman.argIf(typeof sslClientKey != 'undefined' && sslClientKey, ['--ssl-client-key', sslClientKey]);
+    let reporterHtmlTemplate = tl.getPathInput('reporterHtmlTemplate', false, true);
+    newman.argIf(typeof reporterHtmlTemplate != 'undefined' && reporterHtmlTemplate, ['--reporter-html-template', reporterHtmlTemplate]);
+    let reporterHtmlExport = tl.getInput('reporterHtmlExport');
+    newman.argIf(typeof reporterHtmlExport != 'undefined' && reporterHtmlExport, ['--reporter-html-export', reporterHtmlExport]);
+    let reporterJsonExport = tl.getInput('reporterJsonExport');
+    newman.argIf(typeof reporterJsonExport != 'undefined' && reporterJsonExport, ['--reporter-json-export', reporterJsonExport]);
+    let reporters = tl.getInput('reporters');
+    newman.argIf(typeof reporters != 'undefined' && reporters, ['-r', reporters]);
+    let delayRequest = tl.getInput('delayRequest');
+    newman.argIf(typeof delayRequest != 'undefined' && delayRequest, ['--delay-request', delayRequest]);
+    let timeoutRequest = tl.getInput('timeoutRequest');
+    newman.argIf(typeof timeoutRequest != 'undefined' && timeoutRequest, ['--timeout-request', timeoutRequest]);
+    let numberOfIterations = tl.getInput('numberOfIterations');
+    newman.argIf(typeof numberOfIterations != 'undefined' && numberOfIterations, ['-n', numberOfIterations]);
+    return newman;
+}
+
 async function run() {
     try {
         tl.debug('executing newman')
         tl.setResourcePath(path.join(__dirname, 'task.json'));
 
-        var newman: trm.ToolRunner = tl.tool(tl.which('newman', true));
-
-        let sslClientCert = tl.getPathInput('sslClientCert', false, true);
-        newman.argIf(typeof sslClientCert != 'undefined' && sslClientCert, ['--ssl-client-cert', sslClientCert]);
-
-        let sslClientKey = tl.getPathInput('sslClientKey', false, true);
-        newman.argIf(typeof sslClientKey != 'undefined' && sslClientKey, ['--ssl-client-key', sslClientKey]);
-
-        let reporterHtmlTemplate = tl.getPathInput('reporterHtmlTemplate', false, true);
-        newman.argIf(typeof reporterHtmlTemplate != 'undefined' && reporterHtmlTemplate, ['--reporter-html-template', reporterHtmlTemplate]);
-
-        let reporterHtmlExport = tl.getInput('reporterHtmlExport');
-        newman.argIf(typeof reporterHtmlExport != 'undefined' && reporterHtmlExport, ['--reporter-html-export', reporterHtmlExport]);
-
-        let reporterJsonExport = tl.getInput('reporterJsonExport');
-        newman.argIf(typeof reporterJsonExport != 'undefined' && reporterJsonExport, ['--reporter-json-export', reporterJsonExport]);
-
-        let reporters = tl.getInput('reporters');
-        newman.argIf(typeof reporters != 'undefined' && reporters, ['-r', reporters]);
-
-        let delayRequest = tl.getInput('delayRequest');
-        newman.argIf(typeof delayRequest != 'undefined' && delayRequest, ['--delay-request', delayRequest]);
-
-        let timeoutRequest = tl.getInput('timeoutRequest');
-        newman.argIf(typeof timeoutRequest != 'undefined' && timeoutRequest, ['--timeout-request', timeoutRequest]);
-
-        let numberOfIterations = tl.getInput('numberOfIterations');
-        newman.argIf(typeof numberOfIterations != 'undefined' && numberOfIterations, ['-n', numberOfIterations]);
-
         let collectionFileSource = tl.getPathInput('collectionFileSource', true, true);
+        var taskSuccess = true;
         if (tl.stats(collectionFileSource).isDirectory()) {
             let contents: string[] = tl.getDelimitedInput('Contents', '\n', true);
             collectionFileSource = path.normalize(collectionFileSource);
@@ -45,19 +40,25 @@ async function run() {
             let matchedPaths: string[] = tl.match(allPaths, contents, collectionFileSource);
             let matchedFiles: string[] = matchedPaths.filter((itemPath: string) => !tl.stats(itemPath).isDirectory());
 
-            console.log(tl.loc('FoundNFiles', matchedFiles.length));
+            console.log("found %d files", matchedFiles.length);
 
             if (matchedFiles.length > 0) {
-                matchedFiles.forEach(async (file: string) => {
+                matchedFiles.forEach((file: string) => {
+                    var newman: trm.ToolRunner = GetToolRunner();
                     newman.arg('run');
                     newman.arg(file);
                     newman.arg(['-e', tl.getPathInput('environment', true, true)]);
 
-                    await newman.exec();
+                    var execResponse = newman.execSync();
+
+                    if (execResponse.code === 1) {
+                        taskSuccess = false;
+                    }
                 });
             }
         }
         else {
+            var newman: trm.ToolRunner = GetToolRunner();
             newman.arg('run');
             newman.arg(collectionFileSource);
             newman.arg(['-e', tl.getPathInput('environment', true, true)]);
@@ -65,7 +66,12 @@ async function run() {
             await newman.exec();
         }
 
-        tl.setResult(tl.TaskResult.Succeeded, "Success");
+        if (taskSuccess) {
+            tl.setResult(tl.TaskResult.Succeeded, "Success");
+        }
+        else {
+            tl.setResult(tl.TaskResult.Failed, "Failed");
+        }
     } catch (err) {
         tl.setResult(tl.TaskResult.Failed, err.message);
     }

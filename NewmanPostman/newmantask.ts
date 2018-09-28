4106d8abade04db1198da576b662baa20fe21722
Carlo Wahlstedt <carlowahlstedt@gmail.com>
2018-09-27 23:35:43 -0400

    Put it back so the tool works...try to fix the path later

--- NewmanPostman/newmantask.ts
+++ NewmanPostman/newmantask.ts
@@ -3,12 +3,14 @@ import tl = require('vsts-task-lib/task');
 import trm = require('vsts-task-lib/toolrunner');
 
 function GetToolRunner(collectionToRun: string) {
-    let pathToNewman = tl.getInput('pathToNewman', false);
-    if (typeof pathToNewman == 'undefined' || pathToNewman.length == 0) {
-        console.info("No specific path to newman");
-        pathToNewman = "newman";
-    }
-    var newman: trm.ToolRunner = tl.tool(tl.which(pathToNewman, true));
+    // let pathToNewman = tl.getInput('pathToNewman', false);
+    // if (typeof pathToNewman == 'undefined' || pathToNewman.length == 0) {
+    //     console.info("No specific path to newman");
+    //     pathToNewman = "newman";
+    // }
+    // var newman: trm.ToolRunner = tl.tool(tl.which(pathToNewman, true));
+
+    var newman: trm.ToolRunner = tl.tool(tl.which("newman", true));
 
     newman.arg('run');
     newman.arg(collectionToRun);
